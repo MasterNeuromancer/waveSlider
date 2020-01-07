47e88538cad771d5a10d30ef0a409f21ddb58cbc
Neuromancer93 <itrejonumber1@gmail.com>
2020-01-06 22:50:29 -0800

    feat: created Wave Slider file, imported to main

---
+++ lib/wave_slider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WaveSlider extends StatefulWidget {
+  @override
+  _WaveSliderState createState() => _WaveSliderState();
+}
+
+class _WaveSliderState extends State<WaveSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello from Wave Slider'),
+    );
+  }
+}
