a2c10b49fc6dcd6ca8f794bc453041abb957c166
Neuromancer93 <itrejonumber1@gmail.com>
2020-01-11 23:38:10 -0800

    feat: gesture detecter + percentage indicator functional

--- lib/wave_slider.dart
+++ lib/wave_slider.dart
@@ -11,15 +11,65 @@ class WaveSlider extends StatefulWidget {
 }
 
 class _WaveSliderState extends State<WaveSlider> {
+  double _dragPosition = 0;
+  double _dragPercentage = 0;
+
+  void _updateDragPosition(Offset val) {
+    double newDragPosition = 0;
+
+    if (val.dx <= 0) {
+      newDragPosition = 0;
+    } else if (val.dx >= widget.width) {
+      newDragPosition = widget.width;
+    } else {
+      newDragPosition = val.dx;
+    }
+
+    setState(() {
+      _dragPosition = newDragPosition;
+      _dragPercentage = _dragPosition / widget.width;
+    });
+  }
+
+  void _onDragUpdate(BuildContext context, DragUpdateDetails update) {
+    RenderBox box = context.findRenderObject();
+    Offset offset = box.globalToLocal(update.globalPosition);
+    _updateDragPosition(offset);
+    print(offset.dx);
+  }
+
+  void _onDragStart(BuildContext context, DragStartDetails start) {
+    RenderBox box = context.findRenderObject();
+    Offset offset = box.globalToLocal(start.globalPosition);
+    _updateDragPosition(offset);
+    print(offset.dx);
+  }
+
+  void _onDragEnd(BuildContext context, DragEndDetails end) {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: GestureDetector(
-      child: Container(
-        width: widget.width,
-        height: widget.height,
-        color: Colors.red,
+      child: GestureDetector(
+        child: Container(
+          width: widget.width,
+          height: widget.height,
+          color: Colors.red,
+          child: Column(
+            children: <Widget>[
+              Text(_dragPosition.toString()),
+              Text(_dragPercentage.toString()),
+            ],
+          ),
+        ),
+        onHorizontalDragUpdate: (DragUpdateDetails update) =>
+            _onDragUpdate(context, update),
+        onHorizontalDragStart: (DragStartDetails start) =>
+            _onDragStart(context, start),
+        onHorizontalDragEnd: (DragEndDetails end) => _onDragEnd(context, end),
       ),
-    ));
+    );
   }
 }
